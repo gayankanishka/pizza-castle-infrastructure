275678e2b92a5620525ee45364825ab17146d75d
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-12-24 17:05:12 +0530

    add new files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,15 @@
 variable "aws_region" {
-    type = string
-    description = "The main AWS region"
+  type        = string
+  description = "The main AWS region"
+  default     = "us-east-2"
 }
 
 variable "eks_cluster_name" {
-    type = string
-    description = "The name of the EKS cluster"
+  type        = string
+  description = "The name of the EKS cluster"
 }
 
 variable "eks_cluster_instance_type" {
-    type = string
-    description = "The EC2 instance type to use for the EKS cluster"
+  type        = string
+  description = "The EC2 instance type to use for the EKS cluster"
 }
