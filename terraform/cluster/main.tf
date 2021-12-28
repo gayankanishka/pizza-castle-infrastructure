fa50b049d3658f9dc8f7f1b6043eb72a6aab8589
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-12-28 22:48:29 +0530

    remove repo creation

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -207,40 +207,30 @@ resource "kubernetes_secret" "main" {
 }
 
 # GitHub
-resource "github_repository" "main" {
-  name       = var.repository_name
-  visibility = var.repository_visibility
-  auto_init  = true
-}
-
-resource "github_branch_default" "main" {
-  repository = github_repository.main.name
-  branch     = var.branch
-}
 
 resource "github_repository_deploy_key" "main" {
   title      = var.cluster_name
-  repository = github_repository.main.name
+  repository = var.repository_name
   key        = tls_private_key.main.public_key_openssh
   read_only  = true
 }
 
 resource "github_repository_file" "install" {
-  repository = github_repository.main.name
+  repository = var.repository_name
   file       = data.flux_install.main.path
   content    = data.flux_install.main.content
   branch     = var.branch
 }
 
 resource "github_repository_file" "sync" {
-  repository = github_repository.main.name
+  repository = var.repository_name
   file       = data.flux_sync.main.path
   content    = data.flux_sync.main.content
   branch     = var.branch
 }
 
 resource "github_repository_file" "kustomize" {
-  repository = github_repository.main.name
+  repository = var.repository_name
   file       = data.flux_sync.main.kustomize_path
   content    = data.flux_sync.main.kustomize_content
   branch     = var.branch
