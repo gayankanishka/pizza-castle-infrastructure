4554c563311c6ec8d713dad84506bd35d102ec7a
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-05 01:26:42 +0530

    refactor: rename aws stuff

--- aws/main.tf
+++ aws/main.tf
@@ -6,7 +6,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 module "eks_cluster" {
-  source        = "./cluster"
+  source        = "./eks"
   cluster_name  = var.eks_cluster_names[terraform.workspace]
   instance_type = var.eks_cluster_instance_types[terraform.workspace]
 }
